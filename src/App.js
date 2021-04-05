365a11fe11da88123612094673b657549445facf
Tom Bear <tom@bearjam.dev>
2021-04-05 17:15:51 +0100

    clipping planes test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from "three"
+import { useControls } from "leva"
 
 function App() {
+  const { a, b, x, y, z, c } = useControls({
+    a: { value: 1, min: -5, max: 5, step: 1 },
+    b: { value: 1, min: -5, max: 5, step: 1 },
+    x: { value: 0, min: -5, max: 5, step: 1 },
+    y: { value: 0, min: -5, max: 5, step: 1 },
+    z: { value: 0, min: -5, max: 5, step: 1 },
+    c: { value: 0, min: -1, max: 1, step: 0.01 },
+  })
+  const clippingPlanes = [[x, y, z, c]].map(
+    ([x, y, z, c]) => new THREE.Plane(new THREE.Vector3(x, y, z), c)
+  )
+  console.log(clippingPlanes)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh>
+      <boxBufferGeometry args={[a, b]} />
+      <meshBasicMaterial color="tomato" clippingPlanes={clippingPlanes} />
+    </mesh>
+  )
 }
 
-export default App;
+export default App
